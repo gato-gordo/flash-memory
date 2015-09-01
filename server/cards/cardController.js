9594f226dc00b4a1a1625df52eedccd7c8564b4f
Ignacio Prado <ignacio@gatogordoweb.com>
2015-09-01 10:12:22 -0500

    Complete implementation of back-end api

--- server/cards/cardController.js
+++ server/cards/cardController.js
@@ -5,12 +5,15 @@ var Card = require('./cardModel.js'),
 module.exports = {
 
   create: function(req, res, next){
-    var front  = req.body.front, back  =  req.body.back;
-    var create = Q.nbind(Card.create, Card);
-    var newCard = {
-      front: front,
-      back: back
-    };
+  	
+    var front  = req.body.front, 
+    		back  =  req.body.back,
+    		create = Q.nbind(Card.create, Card),
+    		newCard = {
+      		front: front,
+      		back: back
+    		};
+
     create(newCard).then(function (card) {
         res.json(card);
       })
@@ -20,15 +23,12 @@ module.exports = {
   },
 
   index: function(req, res, next){
-  	var findAll = Q.nbind(Card.find, Card);
-  	findAll({})
+
+  	var index = Q.nbind(Card.find, Card);
+
+  	index({})
       .then(function (cards) {
-        if (!cards) {
-          next(new Error('Cards cannot be retrieved.'));
-        } else {
-        	console.log("Successful query", cards);
-    			res.json(cards);
-        }
+    		res.json(cards);
       })
       .fail(function (error) {
         next(error);
@@ -36,16 +36,13 @@ module.exports = {
   },
 
   read: function(req, res, next){
-  	var front = req.url.slice(1);
-    var findCard = Q.nbind(Card.findOne, Card);
 
-    findCard({front: front})
+  	var id = req.params.card_id,
+  			read = Q.nbind(Card.findById, Card);
+
+    read(id)
       .then(function (card) {
-        if (!card) {
-          next(new Error('Card cannot be retrieved.'));
-        } else {
     			res.json(card);
-        }
       })
       .fail(function (error) {
         next(error);
@@ -54,68 +51,31 @@ module.exports = {
 
   update: function(req, res, next){
   	
-    var id = req.params.card_id, front = req.body.front, back  = req.body.back;
-    console.log(req.body);
+	  var id = req.params.card_id, 
+	  		front = req.body.front, 
+	  		back  = req.body.back,
+	  		update = Q.nbind(Card.findByIdAndUpdate, Card);
 
-    var findCard = Q.nbind(Card.findOne, Card);
-
-    /*
-    findCard({_id: id})
-    	.then(function(card){
-        if (!card) {
-          next(new Error('Card cannot be retrieved.'));
-        } else {
-        	console.log('First console', card)
-        	var save = Q.nbind(Card.save, Card);
-    			card.front = front;
-    			card.back = back;
-    			console.log("Second console", card);
-    			save(card);
-        }
-      })
-      .then(function(card){
-      	if (!card) {
-          next(new Error('Card cannot be modified.'));
-        } else {
-        	res.json(card);
-        }
-      })
-      .fail(function (error) {
-        next(error);
-      });*/
-
-    
-    var findOneAndUpdate = Q.nbind(Card.findOneAndUpdate, Card);
-
-   findOneAndUpdate({"_id": id}, {"front": front, "back": back}, {"new": true})
-    .then(function(card) {
-      res.json(card);
-    })
-    .fail(function (error) {
-      next(error);
-    });
+	  update(id, {"front": front, "back": back}, {"new": true})
+	    .then(function(card) {
+	      res.json(card);
+	    })
+	    .fail(function (error) {
+	      next(error);
+	    });
   },
 
-  destroy: function(id){
-  	/*
-  	var id = req.url.id;
+  destroy: function(req, res, next){
 
-  	var findOne = Q.nbind(Card.findOne, Card);
-   findOne({id: id})
-    .then(function(card) {
-      if (!card) {
-        next(new Error('Card cannot be retrieved!'));
-      } else {
-     		var destroy = Q.nbind(Card.destroy, Card);
-      	destroy({id: id});
-      }
-    })
-    .then(function (card) {
-      res.json({card});
-    })
-    .fail(function (error) {
-      next(error);
-    });*/
-  }
-
+    var id = req.params.card_id,
+    		destroy = Q.nbind(Card.findByIdAndRemove, Card);
+  
+    destroy(id)
+      .then(function (card) {
+        res.json(card);
+      })
+      .fail(function (error) {
+       next(error);
+    	});
+   }
 };
